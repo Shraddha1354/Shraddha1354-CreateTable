2512c60299b76f37950ff860bf6275bf5fa643f0
Shraddha1354 <shraddhadhende423@gmail.com>
2023-10-13 12:00:35 +0530

    done

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE Song (
+    title VARCHAR(100),
+    artist VARCHAR(100)
+);
+Insert into Song values('Let it be', 'Beatles'),
+('Hotel California', 'Eagles'),
+('Kashmir', 'Led Zeppelin');
